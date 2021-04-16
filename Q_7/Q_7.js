000d0efe1a48caf8888638b8536759cc287e33b7
VenkataSatyakoppula <60220673+VenkataSatyakoppula@users.noreply.github.com>
2021-04-16 10:20:25 +0530

    Q_7_incomplete

---
+++ Q_7/Q_7.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.sub').on('click',function(){
+        var usr=$('#user').val();
+        var c1=$('v1').val();
+        var c2=$('v2').val();
+        var c3=$('v3').val();
+        var c4=$('v4').val();
+        
+    });
+
+});
